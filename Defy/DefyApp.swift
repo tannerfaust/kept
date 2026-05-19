3cff2aab8c0fe9e5726c8486251eaf668729b289
tannerfaust <tannerwellspoken@proton.me>
2026-05-20 01:53:21 +0200

    Initial Commit

---
+++ Defy/DefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefyApp.swift
+//  Defy
+//
+//  Created by Tanner Fause on 20.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
